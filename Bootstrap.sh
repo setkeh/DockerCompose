7eb0f4a68cf219e03b6f46cbdc4b84b3fcbd635b
James Griffis <setkeh@setkeh.com>
2017-03-31 23:44:40 +1100

    Adding the biginnings of bootstrapping for the Repo

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-#Wip
+# Here we Define the Cluster Hosts (This is a Carppy solution but im not dealing with hundreds of hosts and its simple.)
+function GetHostIp {
+  hostname=$(hostname)
+
+  if [ "$hostname" = "Elenaril" ]
+  then
+    ip="10.99.0.11"
+  elif [ "$hostname" = "Lyra" ]
+  then
+    ip="10.99.0.12"
+  elif [ "$hostname" = "Shyilia" ]
+  then
+    ip="10.99.0.13"
+  else
+    echo "Could not Set Host ip exiting"
+    exit 1
+  fi
+
+}
+
+# This Set the Environment file for CoreOS in /etc/environment
+function SetEnvironment {
+  # Overwrite The Current Environment File.
+  mv ServiceConfigs/environment /etc/environment
+
+  sed -e "s/\${ip}/$ip/" /etc/environment
+}
+
+# Time to actually do all the things
+GetHostIp
+SetEnvironment
